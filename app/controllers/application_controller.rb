26b124c7b7bf77ab6c6bd4f4a5346cbd928735ae
Steven Loar <sjl7909@gmail.com>
2020-12-15 15:56:18 -0500

    Add User name and Guest User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
+include DeviseWhitelist
+include CurrentUserConcern
+
 class ApplicationController < ActionController::Base
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :password, :password_confirmation, presence: true, on: :create
+  validates :password, confirmation: true
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
